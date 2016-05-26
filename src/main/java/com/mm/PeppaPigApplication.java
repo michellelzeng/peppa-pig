e66fed0ba34ba83b55c38b8365005d9b303604d2
Michelle Zeng <mzeng@atlassian.com>
2016-05-26 17:11:16 +1000

    Set up Rest

---
+++ src/main/java/com/mm/PeppaPigApplication.java
@@ -0,0 +1,12 @@
+package com.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeppaPigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeppaPigApplication.class, args);
+	}
+}
