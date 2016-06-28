63a1cfcecef89d70257ad799b57f304f5046f70f
Michelle Zeng <mzeng@atlassian.com>
2016-06-28 16:43:21 +1000

    test getAllPost in descending order

--- src/main/java/com/hello/GreetingController.java
+++ src/main/java/com/hello/GreetingController.java
@@ -1,9 +1,13 @@
 package com.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -11,10 +15,16 @@ public class GreetingController {
 
     private static final String template = "Helloaaasdfsdf, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private PiggiePostRepository repository;
+
+    @Autowired
+    public GreetingController(PiggiePostRepository repository) {
+
+        this.repository = repository;
+    }
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    public Iterable<PiggiePost> greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return repository.findAll(new Sort(new Sort.Order(Sort.Direction.DESC, "id")));
     }
 }
--- src/main/java/com/hello/PiggiePostRepository.java
+++ src/main/java/com/hello/PiggiePostRepository.java
@@ -1,13 +1,14 @@
 package com.hello;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "piggiepost", path = "piggiepost")
-public interface PiggiePostRepository extends CrudRepository<PiggiePost, Long> {
+public interface PiggiePostRepository extends PagingAndSortingRepository<PiggiePost, Long> {
 
     List<PiggiePost> findByTitle(@Param("title") String title);
 }
